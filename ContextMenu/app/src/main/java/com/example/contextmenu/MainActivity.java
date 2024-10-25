60c20844cf4aa1107d44d802ffcc869d83ff3f2d
Chaithra S Nayak <nayakchaithra5@gmail.com>
2024-10-25 21:42:23 +0530

    style: update UI

--- ContextMenu/app/src/main/java/com/example/contextmenu/MainActivity.java
+++ ContextMenu/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -1,22 +1,44 @@
 package com.example.contextmenu;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mytextView;
+    ConstraintLayout mylayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mylayout=findViewById(R.id.layout);
+        mytextView=findViewById(R.id.textView);
+        registerForContextMenu(mytextView);
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Choose any below color");
+        menu.add("YELLOW");
+        menu.add("GREEN");
+        menu.add("MAGENTA");
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getTitle()=="YELLOW")
+            mylayout.setBackgroundColor(Color.YELLOW);
+        if (item.getTitle()=="GREEN")
+            mylayout.setBackgroundColor(Color.GREEN);
+        if (item.getTitle()=="MAGENTA")
+            mylayout.setBackgroundColor(Color.MAGENTA);
+        return true;
     }
 }
