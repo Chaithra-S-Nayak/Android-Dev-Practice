90ed9c02bf88ecc7f09398406bd3206214541615
Chaithra S Nayak <nayakchaithra5@gmail.com>
2024-10-26 19:59:31 +0530

    Camera

---
+++ Camera/app/src/main/java/com/example/camera/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView myImage;
+    Button btn;
+    static int capturedImage = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        myImage = findViewById(R.id.imageView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,capturedImage);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == capturedImage){
+            Bitmap img = (Bitmap)data.getExtras().get("data");
+            myImage.setImageBitmap(img);
+        }
+    }
+}
