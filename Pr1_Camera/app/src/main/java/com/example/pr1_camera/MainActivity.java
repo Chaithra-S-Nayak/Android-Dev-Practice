7eb87303736d0ca011ed6abe30788b00ecba48ca
Chaithra S Nayak <nayakchaithra5@gmail.com>
2024-10-27 17:49:19 +0530

    Pr1 Camera

---
+++ Pr1_Camera/app/src/main/java/com/example/pr1_camera/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pr1_camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+    public static int captureImage=123;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        imageView=findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,captureImage);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==captureImage){
+            Bitmap img= (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(img);
+        }
+    }
+}
