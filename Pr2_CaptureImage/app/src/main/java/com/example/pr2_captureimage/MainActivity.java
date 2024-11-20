e4b1b9f8e122b78f1ca7aa88f1eaaef293500100
Chaithra S Nayak <nayakchaithra5@gmail.com>
2024-11-20 16:59:42 +0530

    Pr2 Capture Image

---
+++ Pr2_CaptureImage/app/src/main/java/com/example/pr2_captureimage/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.pr2_captureimage;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+
+    public static int captureImage=123;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        imageView=findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,captureImage);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==captureImage){
+            Bitmap bitmap=(Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
